f7c51672cd207e571fb3d9e800a5dfc159922c14
emillyavelar <163145098+emillyavelar@users.noreply.github.com>
2024-10-21 07:48:22 -0300

    salvar

---
+++ script.js
@@ -0,0 +1,14 @@
+function contar(string){
+    let numero = 0;
+
+    for(let i = 0; i < string.length; i++){
+        if(string[i] != " " ){
+            numero++;
+        }
+    }
+    return numero;
+}
+
+function palíndromo(string){
+    let 
+}
